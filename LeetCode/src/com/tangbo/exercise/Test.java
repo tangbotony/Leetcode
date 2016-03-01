e9ddd50d674c6096f62fbdc3462d4bf4d30a0b63
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-01 14:55:36 +0800

    292. Nim Game My Submissions Question and solution

---
+++ LeetCode/src/com/tangbo/exercise/Test.java
@@ -0,0 +1,12 @@
+package com.tangbo.exercise;
+
+import com.tangbo.exercise.nimgame.Solution;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.canWinNim(5));
+	}
+
+}
