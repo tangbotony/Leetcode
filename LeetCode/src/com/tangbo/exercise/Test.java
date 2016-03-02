3578a16c50c6460797ac59edbd0fd89f8ce23083
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-02 18:06:53 +0800

    171 Excel Sheet Column Number

--- LeetCode/src/com/tangbo/exercise/Test.java
+++ LeetCode/src/com/tangbo/exercise/Test.java
@@ -1,12 +1,12 @@
 package com.tangbo.exercise;
 
-import com.tangbo.exercise.validanagram.*;
+import com.tangbo.exercise.excelsheetcolumnnumber.*;
 
 public class Test {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.isAnagram("anagram", "nagaram"));
+		System.out.println(solution.titleToNumber("AA"));
 	}
 
 }
