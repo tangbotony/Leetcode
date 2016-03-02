ee19017c90485373dbcea0efbc4f18773112f44d
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-02 15:16:13 +0800

    283. Move Zeroes

--- LeetCode/src/com/tangbo/exercise/Test.java
+++ LeetCode/src/com/tangbo/exercise/Test.java
@@ -1,12 +1,13 @@
 package com.tangbo.exercise;
 
-import com.tangbo.exercise.nimgame.Solution;
+import com.tangbo.exercise.movezeroes.Solution;
 
 public class Test {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		System.out.println(solution.canWinNim(5));
+		int [] a= {0, 1, 0, 3, 12,0,0,3,6,7};
+		solution.moveZeroes(a);
 	}
 
 }
