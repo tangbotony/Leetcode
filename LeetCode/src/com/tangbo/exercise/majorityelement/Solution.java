62ca07092a399bbe472ae3dc151ba4257438299d
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-02 20:14:21 +0800

    add exercise problems

--- LeetCode/src/com/tangbo/exercise/majorityelement/Solution.java
+++ LeetCode/src/com/tangbo/exercise/majorityelement/Solution.java
@@ -1,5 +1,11 @@
 package com.tangbo.exercise.majorityelement;
-
+/*
+ * 169. Majority Element My Submissions Question
+	Total Accepted: 97031 Total Submissions: 241394 Difficulty: Easy
+	Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+	
+	You may assume that the array is non-empty and the majority element always exist in the array.
+ */
 import java.util.HashMap;
 import java.util.Map;
 
