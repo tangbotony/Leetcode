c0f8574f71fddb42a9bf81e2db30069f32b085a1
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-03 11:39:34 +0800

    206Reverse Linked List

---
+++ LeetCode/src/com/tangbo/exercise/reverselinkedlist/Solution.java
@@ -0,0 +1,47 @@
+package com.tangbo.exercise.reverselinkedlist;
+/*
+	206. Reverse Linked List My Submissions Question
+	Total Accepted: 88536 Total Submissions: 230461 Difficulty: Easy
+	Reverse a singly linked list.
+	
+	click to show more hints.
+	
+	Hint:
+	A linked list can be reversed either iteratively or recursively. Could you implement both?
+	
+	Subscribe to see which companies asked this question
+ */
+import java.util.Stack;
+public class Solution {
+	Stack<ListNode> stack = new Stack<>();
+	public ListNode reverseList(ListNode head) {
+		if(head.next==null || head == null)
+			return head;
+		while(head.next!=null)
+		{
+			stack.push(head);
+			head = head.next;
+		}
+		ListNode reverseHead = new ListNode(head.val);
+		head = stack.pop();
+		reverseHead.next = head;
+		while(!stack.isEmpty())
+		{
+			head.next = stack.pop();
+			head = head.next;
+		}
+		head.next = null;
+        return reverseHead;
+    }
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		ListNode result = new Solution().reverseList(head);
+		while(result!=null)
+		{
+			System.out.print(result.val+"	");
+			result = result.next;
+		}
+	}
+}
