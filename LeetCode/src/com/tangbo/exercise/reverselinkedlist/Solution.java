364f6a09db85a87f1981fe4ae0a9fd0c463c9eba
唐波 <tangbo@tangbodeMacBook-Pro.local>
2016-03-03 11:41:25 +0800

    Modify

--- LeetCode/src/com/tangbo/exercise/reverselinkedlist/Solution.java
+++ LeetCode/src/com/tangbo/exercise/reverselinkedlist/Solution.java
@@ -33,15 +33,4 @@ public class Solution {
 		head.next = null;
         return reverseHead;
     }
-	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		head.next = new ListNode(2);
-		head.next.next = new ListNode(3);
-		ListNode result = new Solution().reverseList(head);
-		while(result!=null)
-		{
-			System.out.print(result.val+"	");
-			result = result.next;
-		}
-	}
 }
