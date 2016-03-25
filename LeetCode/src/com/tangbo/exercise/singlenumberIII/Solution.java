7db863347760e957542ddce15f82ac0858ac20b0
Tony.TangBo <tangboneu@foxmail.com>
2016-03-25 19:16:05 +0800

    Single Number III

---
+++ LeetCode/src/com/tangbo/exercise/singlenumberIII/Solution.java
@@ -0,0 +1,33 @@
+package com.tangbo.exercise.singlenumberIII;
+/*
+ * 260. Single Number III My Submissions Question
+	Total Accepted: 29203 Total Submissions: 67689 Difficulty: Medium
+	Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+	
+	For example:
+	
+	Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+	
+	Note:
+	The order of the result is not important. So in the above example, [5, 3] is also correct.
+	Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+	Credits:
+	Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+	
+	Subscribe to see which companies asked this question
+ */
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        int result[] = new int[2];
+        int re = 0;
+        for(int i : nums)
+            re ^= i;
+        int n = re & (~(re-1));
+        for(int i : nums)
+            if((i & n) == 0)
+                result[0] ^= i;
+            else
+                result[1] ^= i;
+        return result;
+    }
+}
